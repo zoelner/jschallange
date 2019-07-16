8b56c1dc26541bc9ccc4c511ff43e883281d47af
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-16 00:09:38 -0300

    Ajuste na regra.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
-    'no-console': ['error', { allow: 'tron' }],
+    'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
