1eb0cb942236c66fa258319ef28e00a45812078e
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-15 23:54:02 -0300

    Ajuste para o componente terminar o loading corretamente.

--- src/store/modules/movie/sagas.js
+++ src/store/modules/movie/sagas.js
@@ -2,27 +2,28 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import * as Types from './types';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
-import { addMovieSuccess, addMovieFailed } from './actions';
+import { addMovieSuccess, addMovieFinally } from './actions';
 
 function* addMovie({ title }) {
   const movieExists = yield select(state =>
-    state.movie.data.find(m => m.title === title)
+    state.movie.data.find(m => m.Title === title)
   );
 
-  if (movieExists) return;
-  try {
-    const response = yield call(api.get, `?apikey=eebcf44c&t=${title}`);
-    if (response.data.Error) {
-      toast.warning('Filme não encontrado!');
-      return;
+  if (movieExists) {
+    toast.info('Você já possui este filme em sua lista!');
+  } else {
+    try {
+      const response = yield call(api.get, `?apikey=eebcf44c&t=${title}`);
+      if (response.data.Error) {
+        toast.warning('Filme não encontrado!');
+      } else {
+        yield put(addMovieSuccess(response.data));
+      }
+    } catch (e) {
+      toast.error('Oopps... Alguma coisa deu errado.');
     }
-
-    yield put(addMovieSuccess(response.data));
-  } catch (e) {
-    toast.error('Oopps... Alguma coisa deu errado.');
-  } finally {
-    yield put(addMovieFailed());
   }
+  yield put(addMovieFinally());
 }
 
 export default all([takeLatest(Types.ADD_REQUEST, addMovie)]);
--- src/store/modules/movie/types.js
+++ src/store/modules/movie/types.js
@@ -4,5 +4,5 @@
 
 export const ADD_REQUEST = '@movie/ADD_REQUEST';
 export const ADD_SUCCESS = '@movie/ADD_SUCCESS';
-export const ADD_FAILED = '@movie/ADD_FAILED';
+export const ADD_FINALLY = '@movie/ADD_FINALLY';
 export const REMOVE = '@movie/REMOVE';
